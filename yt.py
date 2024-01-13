69de93db154fec3467f4f74a514a504b126ce68d
Mohamed Hashemi <dothashemi@gmail.com>
2024-01-13 14:21:52 +0330

    fix again

--- yt.py
+++ yt.py
@@ -8,21 +8,23 @@ from local import *
 def download(link: str):
     file_name = random.randint(100000000, 999999999)
 
-    yt_opts = {
-        'format': 'bestvideo[height<=720,ext=mp4]+bestaudio/best[height<=720,ext=mp4]',
+    options = {
+        'format': 'bestvideo+bestaudio/best',
         'outtmpl': f'{PATH}{file_name}.%(ext)s',
+        'postprocessors': [{
+            'key': 'FFmpegVideoConvertor',
+            'preferedformat': 'mp4',
+        }],
     }
 
     try:
-        with yt_dlp.YoutubeDL(yt_opts) as ydl:
-            info = ydl.extract_info(link, download=False)
+        with yt_dlp.YoutubeDL(options) as ydl:
+            info = ydl.extract_info(link, download=True)
             video_title = info.get('title', 'Untitled Video')
 
             print(f'Title: {video_title}')
 
-            ydl.download([link])
-            
             return video_title, f"{file_name}.mp4"
-        
+    
     except:
             return "Failed!", "Failed!"
